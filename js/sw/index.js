ade3c7cc9b50f8a6b961c8ff95d7db2a2de48ec0
Ian King <ian_king@comcast.net>
2018-06-10 11:34:23 -0400

    feat: sw fetch event

--- js/sw/index.js
+++ js/sw/index.js
@@ -1,4 +1,4 @@
-var staticCacheName = 'restaurant-app-v1';
+var staticCacheName = 'restaurant-app-v2';
 
 self.addEventListener('install', function(event) {
 	event.waitUntil(
@@ -40,10 +40,10 @@ self.addEventListener('activate', function(event) {
 	);
 });
 
-// self.addEventListener('fetch', function(event) {
-// 	event.respondWith(
-// 		caches.match(event.request).then(function(response) {
-// 			return response || fetch(event.request);
-// 		})
-// 	);
-// });
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request).then(function(response) {
+			return response || fetch(event.request);
+		})
+	);
+});
