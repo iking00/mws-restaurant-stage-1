4b22344c47c298508fe94fa6e3e82a02e28955f5
Ian King <ian_king@comcast.net>
2018-06-10 13:14:27 -0400

    feat: add aria-lable to links to convey meaning to view details of restaurant

--- js/main.js
+++ js/main.js
@@ -166,6 +166,7 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute('aria-label','View Details About ' + restaurant.name);
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
